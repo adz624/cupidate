e178c0d2675c650ce26c3bf2622501043d2843d7
eddie <eddie@visionbundles.com>
2015-01-15 16:01:20 +0800

    在 seed 中增加產品

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+@p1 = Product.create(title: '普通', price: 1000)
+@p2 = Product.create(title: '中級', price: 2000)
+@p3 = Product.create(title: '高級', price: 3000)
 
-User.create(email: 'eddie@visionbundles.com', password: '88888888', password_confirmation: '88888888')
+User.create(email: 'eddie@visionbundles.com', 
+  password: '88888888', 
+  password_confirmation: '88888888', 
+  gender: :male, product: @p2)
