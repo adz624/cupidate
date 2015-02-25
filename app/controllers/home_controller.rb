2e594f02f797f7de2e11157fce093b66f8e38c71
eddie <eddie@visionbundles.com>
2015-02-25 17:53:33 +0800

    Add find_* example

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,27 @@
 class HomeController < ApplicationController
   # GET /
   def index
+    @users = User.all
+  end
+
+  # GET /search
+  def search
+    search_email = params[:search][:email]
+
+    # @user = User.where(email: email).first
+
+    # if @user.present?
+    #   redirect_to root_path, notice: "找到用戶：#{@user.inspect}"
+    # else
+    #   redirect_to root_path, alert: "Not found"
+    # end
+
+    @user = User.find_by(email: search_email)
+
+    if @user.present?
+      redirect_to root_path, notice: "找到用戶：#{@user.inspect}"
+    else
+      redirect_to root_path, alert: "Not found"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   root 'home#index'
+
+  get 'search' => 'home#search'
 end
